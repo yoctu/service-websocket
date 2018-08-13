1e18ac8feeef145eb7d82a4f4c3332cabe195381
Jérôme Schaeffer <jer.schaeffer@gmail.com>
2018-08-13 14:38:26 +0200

    First iteration of Yocty websocket services

---
+++ app.js
@@ -0,0 +1,35 @@
+const redis = require('redis'),
+    config = require('config'),
+    http = require('http').createServer((req, res) => {res.writeHead(200);res.end('Ok')}),
+    io = require('socket.io')(http)
+;
+
+let redisClient = redis.createClient(config.get('redis'));
+
+config.get('services').forEach(e => {
+    redisClient.psubscribe(e.channel);
+});
+
+config.get('services').forEach((service) => {
+    io
+        .of(`/${service.service}`)
+        .on('connection', function (socket) {
+           console.info(`connection on ${service.service} channel : ${socket.id}`);
+           redisClient.on("pmessage", function (pattern, channel, message) {
+               const service = config.get('services').find(e => e.channel === pattern);
+
+               // if  the reddis event is not for this service, we skip it
+               if(!pattern.includes(`${service.entity}`)) {
+                   return;
+               }
+
+               const msg = JSON.parse(message);
+
+               //message are send in volatile mode, so no client will be locked waiting for others
+               socket.volatile.emit(channel, msg);
+           });
+   })
+});
+
+const port = config.get('application.port');
+http.listen(port, () => console.log('app listening on ' + port));
